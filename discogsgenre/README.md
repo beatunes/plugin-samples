7d5e816876d0dcc2594508c0a9c7d83624cf86b4
Hendrik Schreiber <hs@tagtraum.com>
2018-05-08 18:35:02 +0200

    First release.

---
+++ discogsgenre/README.md
@@ -0,0 +1,34 @@
+# README
+
+## beaTunes plugin to import genre tags from Discogs
+
+This plugin requires [beaTunes](http://www.beatunes.com/) 5.1.5 or later.
+It will not work with earlier versions.
+
+
+## Installation
+
+To install, place the jar (`discogsgenre-x.y.z.jar`) into beaTunes'
+plugin folder, remove older versions of the plugin and restart beaTunes.
+
+- Windows: `c:\Users\[username]\AppData\Local\tagtraum industries\beaTunes\plugins`
+- macOS: `~/Library/Application Support/beaTunes/Plug-Ins`
+
+
+## Building
+
+To compile from source, install [Maven 3](http://maven.apache.org/) and execute
+
+    mvn clean install
+
+from the directory that the file `pom.xml` is located in.
+You will find the resulting jar file in the `target` subdirectory.
+
+
+## More
+
+For change notes and other plugin-specific infos, please see the plugin descriptor
+[`src/main/resources/META-INF/plugin.xml`](https://raw.githubusercontent.com/beatunes/plugin-samples/master/discogsgenre/src/main/resources/META-INF/plugin.xml).
+For more info on developing plugins, please visit http://www.beatunes.com/beatunes-plugin-api.html
+
+Enjoy.
---
+++ discogsgenre/src/main/java/com/beatunes/discogsgenre/DiscogsGenre.java
@@ -0,0 +1,125 @@
+/*
+ * =================================================
+ * Copyright 2018 tagtraum industries incorporated
+ * All rights reserved.
+ * =================================================
+ */
+package com.beatunes.discogsgenre;
+
+import com.tagtraum.audiokern.AudioId;
+import com.tagtraum.audiokern.AudioSong;
+import com.tagtraum.beatunes.analysis.SongAnalysisTask;
+import com.tagtraum.beatunes.analysis.Task;
+import com.tagtraum.beatunes.messages.Message;
+import com.tagtraum.beatunes.onlinedb.ReferenceSong;
+import com.tagtraum.ubermusic.discogs.Discogs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.Entity;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Imports genre tags from Discogs, if possible.
+ *
+ * @author <a href="mailto:hs@tagtraum.com">Hendrik Schreiber</a>
+ */
+
+// ============================================================================== //
+// It is *essential* for this class to be annotated as Entity.                    //
+// Otherwise it will not be saved in the analysis queue and cannot be processed.  //
+// ============================================================================== //
+@Entity
+public class DiscogsGenre extends SongAnalysisTask {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DiscogsGenre.class);
+
+    public DiscogsGenre() {
+        // this task does not take long - therefore we ignore it in per task progress bars
+        setProgressRelevant(false);
+        // signal to beaTunes that we want it to lookup reference songs
+        setUseOnlineResources(true);
+    }
+
+    /**
+     * Returns a verbose description of the task in HTML format. This is shown in the
+     * Analysis Options dialog (left pane).
+     *
+     * @return verbose HTML description.
+     */
+    @Override
+    public String getDescription() {
+        return "<h1>Discogs Genre</h1><p>Imports genre tags from <a href=\"https://www.discogs.com\">Discogs</a>. Existing values will be overwritten.</p>";
+    }
+
+    /**
+     * This will be the displayed name of the analysis task.
+     *
+     * @return HTML string
+     */
+    @Override
+    public String getName() {
+        return "<html>Import<br>Discogs genre</html>";
+    }
+
+    /**
+     * Attempt to import Discogs ids from {@link com.tagtraum.beatunes.onlinedb.OnlineDB}.
+     *
+     * @param task task
+     */
+    @Override
+    public void processBefore(final Task task) {
+        // referenceSong contains reference values from OnlineDB.
+        final ReferenceSong referenceSong = getReferenceSong();
+        if (referenceSong != null) {
+            // import discogs ids to local song object
+            getSong().getAlbumIds().addAll(referenceSong.getAlbumIds()
+                .stream()
+                .filter(id -> id.getGeneratorName().equals(AudioId.DISCOGS_MASTER_URL)
+                    || id.getGeneratorName().equals(AudioId.DISCOGS_RELEASE_URL))
+                .collect(Collectors.toSet()));
+        }
+        // make sure that we still execute the method runBefore()
+        setSucceeded(false);
+    }
+
+    /**
+     * This is where the actual work occurs. The method is called by beaTunes when
+     * this task is processed in the analysis/task queue after the OnlineDB lookup happened.
+     *
+     * @param task task
+     */
+    @Override
+    public void runBefore(final Task task) {
+        final AudioSong song = getSong();
+        // check whether we can skip this step altogether
+        if (skip()) {
+            if (LOG.isDebugEnabled()) LOG.debug("Skipped " + song);
+            return;
+        }
+        try {
+            // get discogs component
+            final Discogs discogs = getApplication().getPluginManager().getImplementation(Discogs.class);
+            final List<AudioSong> songs = discogs.lookup(song);
+            if (!songs.isEmpty()) {
+                final AudioSong firstMatch = songs.get(0);
+                if (firstMatch.getGenre() != null && !firstMatch.getGenre().trim().isEmpty()) {
+                    final String discogsGenre = firstMatch.getGenre().trim();
+                    if (LOG.isDebugEnabled()) LOG.debug("Discogs genre for " + song + ": " + discogsGenre);
+                    song.setGenre(discogsGenre);
+                } else {
+                    if (LOG.isDebugEnabled()) LOG.debug("Found no discogs genre for " + song);
+                }
+            } else {
+                if (LOG.isDebugEnabled()) LOG.debug("Failed to find track on discogs: " + song);
+            }
+        } catch (Exception e) {
+            LOG.error("Failed to fetch discogs genre for " + song, e);
+            final String message = e.getLocalizedMessage() == null ? e.toString() : e.getLocalizedMessage();
+            getMessagePanel().addMessage(new Message(getApplication().localize("Analysis"),
+                "Failed to find genre tag for '" + song.getName() + "': " + message, song.getId()));
+        }
+    }
+
+}
